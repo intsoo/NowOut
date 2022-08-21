6e192afadf561bc6c1fc70580ab478343bcbd20d
sforseohn <sforseohn@ewhain.net>
2022-08-22 08:01:53 +0900

    0822 오류수정

---
+++ NowOut2022/Assets/Inventory_KSH/Scripts/UseItem/UseItem2_KSH.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEditor;
+
+
+public class UseItem2_KSH : MonoBehaviour
+{
+    public bool isTouchSister = false;
+    private obj2ButtonTouch_KSH bool_script;
+    public GameObject Btn;
+    public GameObject objCanvas;
+    private void Start()
+    {
+        bool_script = Btn.GetComponent<obj2ButtonTouch_KSH>();
+    }
+
+    void Update()
+    {
+        if (bool_script.obj2_1BtnTouch)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                CheckTouchClear();
+
+            }
+        }
+        // 주석 처리한 부분이 터치 인식인데 테스트해보기 위해 마우스 입력을 적음.
+        //foreach (Touch touch in Input.touches)
+        //{
+        //    if (touch.phase == TouchPhase.Began)
+        //    CheckTouchClear();
+    }
+
+    void CheckTouchClear()
+    {
+        TouchSister();
+        if (isTouchSister)
+        {
+            DataController.Instance.gameData.Ep1_Clear++;
+            DataController.Instance.gameData.Ep1_obj1 = 0;
+            DataController.Instance.gameData.Ep1_obj1Order = 0;
+            bool_script.obj2_1BtnTouch = false;
+            objCanvas.SetActive(false);
+        }
+
+    }
+
+    void TouchSister()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag("Sister"))
+                isTouchSister = true;
+        }
+
+        //Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    if (hit.collider == GetComponent<BoxCollider>())
+        //    {
+        //        isTouchSister = true;
+        //    }
+        //}
+    }
+}
+
+
